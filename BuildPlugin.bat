8667f633de57f31e1c39111cfb2ccdeb6e98f98e
victorhmrod <vhcgames1@gmail.com>
2024-06-17 00:33:30 -0300

    Update BuildPlugin.bat

--- BuildPlugin.bat
+++ BuildPlugin.bat
@@ -4,9 +4,9 @@ set EngineVersion=5.3
 
 for /f "skip=2 tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\%EngineVersion%" /v "InstalledDirectory"') do set "EngineDirectory=%%b"
 
-set AutomationToolPath="C:\Program Files\EpicGames\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat"
+set AutomationToolPath="C:\Program Files\EpicGames\UE_5.4\Engine\Build\BatchFiles\RunUAT.bat"
 set PluginPath="E:\Unreal Projects\Sandbox\Plugins\ReplicatedExplosionPlugin\ReplicatedExplosionPlugin.uplugin"
-set OutputPath="C:\Users\Henrique\Downloads\REP\Build\5.3"
+set OutputPath="C:\Users\Henrique\Downloads\REP\Build\5.4"
 
 title Build Plugin
 echo Automation Tool Path: %AutomationToolPath%
